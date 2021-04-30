876b6f10ce1664e7aa087558f80458c0d1ef7cc6
eric <lee.eric.03@gmail.com>
2021-04-30 16:36:57 -0600

    commit tree ac129eabd631896c86326e528479c1592bba0c2e

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,10 @@
 # Build your say_hello method here
+def restaurant 
+    restaurant_name = "Guy's American Kitchen & Bar"
+    cuisine = "american"
+    motto = "Welcome to Flavor Town!"
+end
+
+def print_name 
+    puts "Guy Fieri"
+end
